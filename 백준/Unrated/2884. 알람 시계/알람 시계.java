d853d8ae7600deec59b28b0a9624447f5e88979f
hyunbinHwang <62677414+hyunbinHwang@users.noreply.github.com>
2023-03-21 18:01:38 +0900

    [Unrated] Title: 알람 시계, Time: 232 ms, Memory: 18584 KB -BaekjoonHub

---
+++ 백준/Unrated/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+
+        if (M < 45) {
+            H--;
+            if (H < 0) {
+                H = 23;
+            }
+            M = 60 - (45 - M);
+        } else {
+            M = M - 45;
+        }
+
+        System.out.println(H + " " + M);
+    }
+}
