0f0efa281b1fff648aa1f109fb89859d2e8b7352
hyunbinHwang <62677414+hyunbinHwang@users.noreply.github.com>
2023-03-21 17:55:55 +0900

    [Unrated] Title: 알파벳 찾기, Time: 232 ms, Memory: 18312 KB -BaekjoonHub

---
+++ 백준/Unrated/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
+        int[] arr = new int[26];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        for (int i = 0; i < S.length(); i++) {
+            if (arr[S.charAt(i) - 'a'] == -1) {
+                arr[S.charAt(i) - 'a'] = i;
+            }
+        }
+
+        for (int i: arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
